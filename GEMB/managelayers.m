e36a31673c12ddc84046789fe7dcda954a61b296
Chad A. Greene <chad@chadagreene.com>
2024-07-12 19:44:43 -0700

    Update managelayers.m
    Significant efficiency gains in the cell-splitting section. Previously, a loop was used to split cells in dz, W, m, T, etc, which resized each of those variables many times. A significant portion of the GEMB processing time was spent on that loop, so I vectorized it and it's much faster now.

--- GEMB/managelayers.m
+++ GEMB/managelayers.m
@@ -106,26 +106,59 @@ for i=2:n
 	end
 end
 
-for j=n:-1:1
-	if (j<X && dz(j) > dzMax2(j)+Dtol) || (dz(j) > dzMax2(j)*zY2+Dtol)
+%% Split cells
+% 
+% % This loop was in Alex Gardner's original code. Chad Greene vectorized
+% % it in July 2024: 
+%
+% for j=n:-1:1
+% 	if (j<X && dz(j) > dzMax2(j)+Dtol) || (dz(j) > dzMax2(j)*zY2+Dtol)
+% 
+% 		% split in two
+% 		dz =    [   dz(1:j-1) ;    dz(j)/2 ;    dz(j)/2 ;    dz(j+1:end)];
+% 		W =     [    W(1:j-1) ;     W(j)/2 ;     W(j)/2 ;     W(j+1:end)];
+% 		m =     [    m(1:j-1) ;     m(j)/2 ;     m(j)/2 ;     m(j+1:end)];
+% 		T =     [    T(1:j-1) ;     T(j)   ;     T(j)   ;     T(j+1:end)];
+% 		d =     [    d(1:j-1) ;     d(j)   ;     d(j)   ;     d(j+1:end)];
+% 		a =     [    a(1:j-1) ;     a(j)   ;     a(j)   ;     a(j+1:end)];
+% 		adiff = [adiff(1:j-1) ; adiff(j)   ; adiff(j)   ; adiff(j+1:end)];
+% 		EI =    [   EI(1:j-1) ;    EI(j)/2 ;    EI(j)/2 ;    EI(j+1:end)];
+% 		EW =    [   EW(1:j-1) ;    EW(j)/2 ;    EW(j)/2 ;    EW(j+1:end)];
+% 		re =    [   re(1:j-1) ;    re(j)   ;    re(j)   ;    re(j+1:end)];
+% 		gdn =   [  gdn(1:j-1) ;   gdn(j)   ;   gdn(j)   ;   gdn(j+1:end)];
+% 		gsp =   [  gsp(1:j-1) ;   gsp(j)   ;   gsp(j)   ;   gsp(j+1:end)];
+% 	end
+% end
 
-		% split in two
-		dz = [dz(1:j-1) ; dz(j)/2 ; dz(j)/2 ; dz(j+1:end)];
-		W = [W(1:j-1) ; W(j)/2 ; W(j)/2 ; W(j+1:end)];
-		m = [m(1:j-1) ; m(j)/2 ; m(j)/2 ; m(j+1:end)];
-		T = [T(1:j-1) ; T(j) ; T(j) ; T(j+1:end)];
-		d = [d(1:j-1) ; d(j) ; d(j) ; d(j+1:end)];
-		a = [a(1:j-1) ; a(j) ; a(j) ; a(j+1:end)];
-		adiff = [adiff(1:j-1) ; adiff(j) ; adiff(j) ; adiff(j+1:end)];
-		EI = [EI(1:j-1) ; EI(j)/2 ; EI(j)/2 ; EI(j+1:end)];
-		EW = [EW(1:j-1) ; EW(j)/2 ; EW(j)/2 ; EW(j+1:end)];
-		re = [re(1:j-1) ; re(j) ; re(j) ; re(j+1:end)];
-		gdn = [gdn(1:j-1) ; gdn(j) ; gdn(j) ; gdn(j+1:end)];
-		gsp = [gsp(1:j-1) ; gsp(j) ; gsp(j) ; gsp(j+1:end)];
-	end
-end
+% The rest of this code section is what Chad Greene wrote to replace the
+% loop above: 
 
-n=length(T);
+% Find the cells that exceed tolerances: 
+f = find((1:n)'<X & ( dz > dzMax2+Dtol) | (dz > dzMax2*zY2+Dtol));
+
+% Conserve quantities among the cells that will be split: 
+dz(f) = dz(f)/2; 
+W(f) = W(f)/2; 
+m(f) = m(f)/2; 
+EI(f) = EI(f)/2; 
+EW(f) = EW(f)/2; 
+
+% Sort the indices of all the cells including the ones that will be duplicated:  
+fs = sort([(1:n)';f]); 
+
+% Recreate the variables with split cells: 
+dz = dz(fs); 
+W = W(fs); 
+m = m(fs); 
+T = T(fs); 
+d = d(fs); 
+a = a(fs); 
+adiff = adiff(fs); 
+EI = EI(fs); 
+EW = EW(fs); 
+re = re(fs); 
+gdn = gdn(fs); 
+gsp = gsp(fs); 
 
 %% CORRECT FOR TOTAL MODEL DEPTH
 % WORKS FINE BUT HAS BEEN DISABLED FOR CONVIENCE OF MODEL OUTPUT
@@ -136,39 +169,45 @@ Ztot = sum(dz);
 
 if Ztot < zMin-Dtol
 
-   % mass and energy to be added
-	mAdd = m(end) + W(end);
-	addE = T(end) * m(end) * CI + W(end) * (LF+CtoK*CI);
-	dz_add=dz(end);
+    % Mass and energy to be added:
+	mAdd   = m(end) + W(end);
+	addE   = T(end) * m(end) * CI + W(end) * (LF+CtoK*CI);
+	dz_add = dz(end);
 
-	% add a grid cell of the same size and temperature to the bottom
-	dz = [dz; dz(end)];
-	T = [T; T(end)];
-	W = [W; W(end)];
-	m = [m; m(end)];
-	d = [d; d(end)];
-	a = [a; a(end)];
+	% Add a grid cell of the same size and temperature to the bottom:
+	dz    = [   dz;    dz(end)];
+	T     = [    T;     T(end)];
+	W     = [    W;     W(end)];
+	m     = [    m;     m(end)];
+	d     = [    d;     d(end)];
+	a     = [    a;     a(end)];
 	adiff = [adiff; adiff(end)];
-	EI = [EI; EI(end)];
-	EW = [EW; EW(end)];
-	re = [re; re(end)];
-	gdn = [gdn; gdn(end)];
-	gsp = [gsp; gsp(end)];
-
-	n=n+1;
+	EI    = [   EI;    EI(end)];
+	EW    = [   EW;    EW(end)];
+	re    = [   re;    re(end)];
+	gdn   = [  gdn;   gdn(end)];
+	gsp   = [  gsp;   gsp(end)];
 
 elseif Ztot > zMax+Dtol
 
-	% mass and energy loss
-	mAdd = -(m(end) + W(end));
-	addE = -(T(end) * m(end) * CI) - W(end) * (LF+CtoK*CI);
-	dz_add=-(dz(end));
+	% Mass and energy loss:
+	mAdd   = -(m(end) + W(end));
+	addE   = -(T(end) * m(end) * CI) - W(end) * (LF+CtoK*CI);
+	dz_add = -(dz(end));
 
-	% remove a grid cell from the bottom
-   dz(end) = []; T(end) = []; W(end) = []; m(end) = [];
-   d(end) = []; a(end) = [];  re(end) = [];  gdn(end) = [];
-	gsp(end) = []; adiff(end) = []; EI(end) = []; EW(end) = [];
+	% Remove a grid cell from the bottom:
+    dz(end)    = []; 
+    T(end)     = []; 
+    W(end)     = []; 
+    m(end)     = [];
+    d(end)     = []; 
+    a(end)     = [];  
+    re(end)    = [];  
+    gdn(end)   = [];
+	gsp(end)   = []; 
+    adiff(end) = []; 
+    EI(end)    = []; 
+    EW(end)    = [];
 
-	n=n-1;
 end
 
