099ebc9db19e0d15f6340372ce603e499586dbbf
NJSchlegel <95945328+NJSchlegel@users.noreply.github.com>
2024-09-13 16:19:26 -0400

    Update testscript.m
    fix error message for layers and add master run to test

--- testscript.m
+++ testscript.m
@@ -1,9 +1,11 @@
-cd GEMB/TEST_GEMBvsISSM
+cd GEMB
+MASTER_RUN
+
+cd TEST_GEMBvsISSM
 testGEMBISSM
 if max_meltbias_with_gemb_matlab
   error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: new melt values do not match archive GEMBtest_output.mat . ')
 end
 if final_layerbias_with_gemb_matlab
-  error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: the run ends with numbers of layers that do not match ...
-    archive GEMBtest_output.mat. ')
+  error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: the run ends with numbers of layers that do not match archive GEMBtest_output.mat. ')
 end
