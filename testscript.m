d0c96ce1170d23b940056ab46d1af30219df565e
NJSchlegel <95945328+NJSchlegel@users.noreply.github.com>
2024-09-13 16:11:50 -0400

    Update testscript.m
    Add test error statements

--- testscript.m
+++ testscript.m
@@ -1,2 +1,9 @@
 cd GEMB/TEST_GEMBvsISSM
 testGEMBISSM
+if max_meltbias_with_gemb_matlab
+  error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: new melt values do not match archive GEMBtest_output.mat . ')
+end
+if final_layerbias_with_gemb_matlab
+  error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: the run ends with numbers of layers that do not match ...
+    archive GEMBtest_output.mat. ')
+end
