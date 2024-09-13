566c96e8e1ed0d61f41e4b109d3134db23d37035
NJSchlegel <95945328+NJSchlegel@users.noreply.github.com>
2024-09-13 17:08:51 -0400

    Update testscript.m
    add test for MASTER_RUN output equality

--- testscript.m
+++ testscript.m
@@ -1,5 +1,7 @@
 cd GEMB
 MASTER_RUN
+bechmark_out = load('../TEST_DATA/S2A1D2_000001_test.mat');
+master_test_out = load('../TEST_DATA/S2A1D2_000001.mat');
 
 cd TEST_GEMBvsISSM
 testGEMBISSM
@@ -8,4 +10,10 @@ if max_meltbias_with_gemb_matlab
 end
 if final_layerbias_with_gemb_matlab
   error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: the run ends with numbers of layers that do not match archive GEMBtest_output.mat. ')
+end
+if ~isequaln(a,b)
+  error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: GEMB output structure differs from GEMBtest_output.mat . ')
+end
+if ~isequaln(bechmark_out,master_test_out)
+  error('FAILURE in GEMB/MASTER_RUN.m: GEMB output structure differs from ../TEST_DATA/S2A1D2_000001_test.mat . ')
 end
