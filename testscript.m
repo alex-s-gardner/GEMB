8dd6d2ccc4e005ed8d3142515e3471762a1d18c0
NJSchlegel <95945328+NJSchlegel@users.noreply.github.com>
2024-09-13 17:33:26 -0400

    Update testscript.m
    Run the short tests first, since the MASTER_RUN takes a while.

--- testscript.m
+++ testscript.m
@@ -1,9 +1,4 @@
-cd GEMB
-MASTER_RUN
-bechmark_out = load('../TEST_DATA/S2A1D2_000001_test.mat');
-master_test_out = load('../TEST_DATA/S2A1D2_000001.mat');
-
-cd TEST_GEMBvsISSM
+cd GEMB/TEST_GEMBvsISSM
 testGEMBISSM
 if max_meltbias_with_gemb_matlab
   error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: new melt values do not match archive GEMBtest_output.mat . ')
@@ -14,6 +9,11 @@ end
 if ~isequaln(a,b)
   error('FAILURE in GEMB/TEST_GEMBvsISSM/testGEMBISSM.m: GEMB output structure differs from GEMBtest_output.mat . ')
 end
+
+cd ../
+MASTER_RUN
+bechmark_out = load('../TEST_DATA/S2A1D2_000001_test.mat');
+master_test_out = load('../TEST_DATA/S2A1D2_000001.mat');
 if ~isequaln(bechmark_out,master_test_out)
   error('FAILURE in GEMB/MASTER_RUN.m: GEMB output structure differs from ../TEST_DATA/S2A1D2_000001_test.mat . ')
 end
