1f732906a05c17a65f9ac9ad869e8609d2c0e2f4
Chad A. Greene <chad@chadagreene.com>
2025-04-30 16:59:26 -0700

    Update testGEMBISSM.m
    Minor adjustments to spacing for readability.

--- GEMB/TEST_GEMBvsISSM/testGEMBISSM.m
+++ GEMB/TEST_GEMBvsISSM/testGEMBISSM.m
@@ -26,31 +26,31 @@ isrestart=false;
 inputs=load('../../TEST_DATA/gemb_input.mat');
 
 % setup the inputs (duplicated for all the elements of an ISSM shelf model in case these are needed):
-smb.Ta=[repmat(inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.V=[repmat(inputs.V0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.dswrf=[repmat(inputs.dsw0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.dlwrf=[repmat(inputs.dlw0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.P=[repmat(inputs.P0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.eAir=[repmat(inputs.eAir0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.pAir=[repmat(inputs.pAir0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.Vz=repmat(inputs.LP.Vz,mdt.mdmesh.numberofelements,1);
-smb.Tz=repmat(inputs.LP.Tz,mdt.mdmesh.numberofelements,1);
-smb.Tmean=repmat(inputs.LP.Tmean,mdt.mdmesh.numberofelements,1);
-smb.C=repmat(inputs.LP.C,mdt.mdmesh.numberofelements,1);
+smb.Ta          = [repmat(inputs.Ta0',     mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.V           = [repmat(inputs.V0' ,     mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.dswrf       = [repmat(inputs.dsw0',    mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.dlwrf       = [repmat(inputs.dlw0',    mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.P           = [repmat(inputs.P0',      mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.eAir        = [repmat(inputs.eAir0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.pAir        = [repmat(inputs.pAir0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.Vz          =  repmat(inputs.LP.Vz,    mdt.mdmesh.numberofelements,1);
+smb.Tz          =  repmat(inputs.LP.Tz,    mdt.mdmesh.numberofelements,1);
+smb.Tmean       =  repmat(inputs.LP.Tmean, mdt.mdmesh.numberofelements,1);
+smb.C           =  repmat(inputs.LP.C,     mdt.mdmesh.numberofelements,1);
 
-smb.dswdiffrf=[repmat(0*inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.cotValue=[repmat(0*inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.ccsnowValue=[repmat(0*inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.cciceValue=[repmat(0*inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
-smb.szaValue=[repmat(0*inputs.Ta0',mdt.mdmesh.numberofelements,1);inputs.dateN'];
+smb.dswdiffrf   = [repmat(0*inputs.Ta0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.cotValue    = [repmat(0*inputs.Ta0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.ccsnowValue = [repmat(0*inputs.Ta0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.cciceValue  = [repmat(0*inputs.Ta0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
+smb.szaValue    = [repmat(0*inputs.Ta0',   mdt.mdmesh.numberofelements,1); inputs.dateN'];
 
 if issza
 	addpath ../../TEST_DATA/SolarAzEl/
 
-	xer=mean(mdt.mdmesh.x(mdt.mdmesh.elements),2);
-	yer=mean(mdt.mdmesh.x(mdt.mdmesh.elements),2);
-	[later,loner]=xy2ll(xer,yer,+1);
-	surfe=mean(mdt.mdgeometry.surface(mdt.mdmesh.elements),2);
+	xer = mean(mdt.mdmesh.x(mdt.mdmesh.elements),2);
+	yer = mean(mdt.mdmesh.x(mdt.mdmesh.elements),2);
+	[later,loner] = xy2ll(xer,yer,+1);
+	surfe = mean(mdt.mdgeometry.surface(mdt.mdmesh.elements),2);
 
 	DateVec=datenum([inputs.dateN(1),1,1,0,0,0])+180/1440*[0:(size(smb.Ta,2)-1)];
 	for ne=1:(size(smb.szaValue,1)-1)
@@ -62,21 +62,21 @@ end
 %In case of restart, take the last values from a previous run and set as *ini values in the smb class
 if isrestart
 
-	tendi=find(cell2mat({mdt.mdresults.time})==max(cell2mat({mdt.mdresults.time})));
-	smb.Dzini=mdt.mdresults(tendi).SmbDz; %cell depth (m)
-	smb.Dini=mdt.mdresults(tendi).SmbD; %snow density (kg m-3)
-	smb.Reini=mdt.mdresults(tendi).SmbRe; %effective grain size (mm)
-	smb.Gdnini=mdt.mdresults(tendi).SmbGdn;  %grain dendricity (0-1)
-	smb.Gspini=mdt.mdresults(tendi).SmbGsp; %grain sphericity (0-1)
-	smb.ECini=mdt.mdresults(tendi).SmbEC; %evaporation/condensation (kg m-2)
-	smb.Wini=mdt.mdresults(tendi).SmbW; %Water content (kg m-2)
-	smb.Aini=mdt.mdresults(tendi).SmbA; %albedo (0-1)
-	smb.Tini=mdt.mdresults(tendi).SmbT; %snow temperature (K) 
-	smb.Adiffini=mdt.mdresults(tendi).SmbAdiff; %albedo, diffusive radiation (0-1) 
-	tend=mdt.mdresults(tendi).time
-	smb.Vmean=mdt.mdsmb.Vmean; %mean annual wind velocity [m s-1]
-	smb.C=mdt.mdsmb.C; %mean annual snow accumulation [kg m-2 yr-1]
-	smb.Tmean=mdt.mdsmb.Tmean; %mean annual temperature [K]
+	tendi        = find(cell2mat({mdt.mdresults.time})==max(cell2mat({mdt.mdresults.time})));
+	smb.Dzini    = mdt.mdresults(tendi).SmbDz;    % cell depth (m)
+	smb.Dini     = mdt.mdresults(tendi).SmbD;     % snow density (kg m-3)
+	smb.Reini    = mdt.mdresults(tendi).SmbRe;    % effective grain size (mm)
+	smb.Gdnini   = mdt.mdresults(tendi).SmbGdn;   % grain dendricity (0-1)
+	smb.Gspini   = mdt.mdresults(tendi).SmbGsp;   % grain sphericity (0-1)
+	smb.ECini    = mdt.mdresults(tendi).SmbEC;    % evaporation/condensation (kg m-2)
+	smb.Wini     = mdt.mdresults(tendi).SmbW;     % Water content (kg m-2)
+	smb.Aini     = mdt.mdresults(tendi).SmbA;     % albedo (0-1)
+	smb.Tini     = mdt.mdresults(tendi).SmbT;     % snow temperature (K) 
+	smb.Adiffini = mdt.mdresults(tendi).SmbAdiff; % albedo, diffusive radiation (0-1) 
+	tend         = mdt.mdresults(tendi).time
+	smb.Vmean    = mdt.mdsmb.Vmean;               % mean annual wind velocity [m s-1]
+	smb.C        = mdt.mdsmb.C;                   % mean annual snow accumulation [kg m-2 yr-1]
+	smb.Tmean    = mdt.mdsmb.Tmean;               % mean annual temperature [K]
 
 	Size_ini=zeros(1,length(mdt.mdresults(tendi).SmbDz(:,1)));
 	for i=1:length(mdt.mdresults(tendi).SmbDz(:,1))
@@ -94,45 +94,45 @@ end
 
 addpath ./
 S=struct(smb);
-S.spinUp = 0;
-S.runID = 'GEMBtest';
-S.outputFreq='daily';
-S.outDIR='./';
+S.spinUp      = 0;
+S.runID       = 'GEMBtest';
+S.outputFreq  = 'daily';
+S.outDIR      = './';
 
 %For the GEMB matlab run, we only need the single point value as input
-S.zTop=S.zTop(1);
-S.dzTop=S.dzTop(1);
-S.dzMin=S.dzMin(1);
-S.zY=S.zY(1);
-S.zMax=S.zMax(1);
-S.zMin=S.zMin(1);
-S.dswdiffrf=S.dswdiffrf(1,:);
-S.szaValue=S.szaValue(1,:);
-S.cotValue=S.cotValue(1,:);
-S.ccsnowValue=S.ccsnowValue(1,:);
-S.cciceValue=S.cciceValue(1,:);
-S.Tmean=S.Tmean(1);
-S.Vmean=S.Vmean(1);
-S.C=S.C(1);
-S.Tz=S.Tz(1);
-S.Vz=S.Vz(1);
-S.aValue=S.aValue(1);
-S.teValue=S.teValue(1);
-S.dulwrfValue=S.dulwrfValue(1);
+S.zTop        = S.zTop(1);
+S.dzTop       = S.dzTop(1);
+S.dzMin       = S.dzMin(1);
+S.zY          = S.zY(1);
+S.zMax        = S.zMax(1);
+S.zMin        = S.zMin(1);
+S.dswdiffrf   = S.dswdiffrf(1,:);
+S.szaValue    = S.szaValue(1,:);
+S.cotValue    = S.cotValue(1,:);
+S.ccsnowValue = S.ccsnowValue(1,:);
+S.cciceValue  = S.cciceValue(1,:);
+S.Tmean       = S.Tmean(1);
+S.Vmean       = S.Vmean(1);
+S.C           = S.C(1);
+S.Tz          = S.Tz(1);
+S.Vz          = S.Vz(1);
+S.aValue      = S.aValue(1);
+S.teValue     = S.teValue(1);
+S.dulwrfValue = S.dulwrfValue(1);
 
 if isrestart
 	%Initialized values for restart
-	S.Dzini=S.Dzini(1,:)';
-	S.Dini=S.Dini(1,:)';
-	S.Reini=S.Reini(1,:)';
-	S.Gdnini=S.Gdnini(1,:)';
-	S.Gspini=S.Gspini(1,:)';
-	S.ECini=S.ECini(1,:)';
-	S.Wini=S.Wini(1,:)';
-	S.Aini=S.Aini(1,:)';
-	S.Tini=S.Tini(1,:)';
-	S.Adiffini=S.Adiffini(1,:)';
-	S.Sizeini=S.Sizeini(1);
+	S.Dzini    = S.Dzini(1,:)';
+	S.Dini     = S.Dini(1,:)';
+	S.Reini    = S.Reini(1,:)';
+	S.Gdnini   = S.Gdnini(1,:)';
+	S.Gspini   = S.Gspini(1,:)';
+	S.ECini    = S.ECini(1,:)';
+	S.Wini     = S.Wini(1,:)';
+	S.Aini     = S.Aini(1,:)';
+	S.Tini     = S.Tini(1,:)';
+	S.Adiffini = S.Adiffini(1,:)';
+	S.Sizeini  = S.Sizeini(1);
 end
 
 lenrun=8*365;
@@ -143,12 +143,12 @@ a=load('GEMBtest.mat');
 
 if ISSM_compare
 
-	md=triangle(model(),'../../TEST_DATA/Square.exp',350000.);
-	md=setmask(md,'all','');
-	md=parameterize(md,'../../TEST_DATA/SquareShelf.par');
-	md=setflowequation(md,'SSA','all');
+	md = triangle(model(),'../../TEST_DATA/Square.exp',350000.);
+	md = setmask(md,'all','');
+	md = parameterize(md,'../../TEST_DATA/SquareShelf.par');
+	md = setflowequation(md,'SSA','all');
 	md.materials.rho_ice=910;
-	md.cluster=generic('name',oshostname(),'np',3);
+	md.cluster = generic('name',oshostname(),'np',3);
 
 	md.smb=smb;
 
@@ -166,17 +166,17 @@ if ISSM_compare
 	md.transient.isthermal=0;
 
 	%time stepping:
-	md.timestepping.start_time=1965;
-	md.timestepping.final_time=1966;
-	md.timestepping.time_step=1/365.0;
-	md.timestepping.interp_forcing=0;
+	md.timestepping.start_time     = 1965;
+	md.timestepping.final_time     = 1966;
+	md.timestepping.time_step      = 1/365.0;
+	md.timestepping.interp_forcing = 0;
 
 	%Run transient
-	md=solve(md,'Transient');
-	M=[md.results.TransientSolution(1).SmbAccumulatedMelt diff([md.results.TransientSolution(:).SmbAccumulatedMelt],[],2)];
+	md = solve(md,'Transient');
+	M = [md.results.TransientSolution(1).SmbAccumulatedMelt diff([md.results.TransientSolution(:).SmbAccumulatedMelt],[],2)];
 
 	%[(1:365)' M(1,:)'*1000*.910 a.M']
-	max_meltbias_with_issm=max(abs([M(1,:)'*1000*.910-a.M']))
+	max_meltbias_with_issm = max(abs([M(1,:)'*1000*.910-a.M']))
 
 	final_layerbias_with_issm=length(md.results.TransientSolution(end).SmbT(1,:)) - a.m(end)
 
