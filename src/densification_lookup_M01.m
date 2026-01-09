f027bd848f5a328e659c14ca8b5010fc30eb911f
Alex Gardner <alex.s.gardner@jpl.nasa.gov>
2026-01-09 10:52:08 -0800

    fix densification_coeffs_M01

--- src/densification_lookup_M01.m
+++ src/densification_lookup_M01.m
@@ -4,9 +4,11 @@ switch densification_coeffs_M01
     % ERA5 new albedo_method="GardnerSharp", sw_absorption_method=0
     case "Ant_ERA5_GS_SW0"
         M01 = [[1.5131, 0.1317, 0.1317, 0.2158]; [1.8422, 0.1688, 2.4979, 0.3225]];
+
     % ERA5 v4 (Paolo et al., 2023)
     case "Ant_ERA5v4_Paolo23"    
         M01 = [2.84, 0.32, 3.10, 0.37];
+
     % ERA5 new albedo_method="BruneLeFebre", sw_absorption_method=1
     case "Ant_ERA5_BF_SW1"
         M01 = [2.2191, 0.2301, 2.2917, 0.2710]; 
@@ -22,17 +24,18 @@ switch densification_coeffs_M01
     % ------------------------- Greenland ------------------------
     % ERA5 new albedo_method="GardnerSharp", sw_absorption_method=0, bare ice
     case "Gre_ERA5_GS_SW0"
-        M01 = [1.3566, 0.1350, 1.8705, 0.2290];
+        M01 = [[1.3566, 0.1350, 1.8705, 0.2290]; [1.4318, 0.1055, 2.0453, 0.2137]];
 
     % RACMO callibration, default (Gardner et al., 2023)
     case "Gre_RACMO_GS_SW0"
-        M01 = [[1.2691, 0.1184, 1.9983, 0.2511]; [1.4318, 0.1055, 2.0453, 0.2137]];
+        M01 = [1.2691, 0.1184, 1.9983, 0.2511];
 
     %  ismember(albedo_method,["GreuellKonzelmann","BougamontBamber"]) && sw_absorption_method>0
     case "Gre_RACMO_GB_SW1"
-        M01 = [2.2191, 0.2301, 2.2917, 0.2710];
+        M01 = [1.7834, 0.1409, 1.9260, 0.1527];
    
     % Kuipers Munneke and others (2015) [semi-emperical], Greenland
     case "Gre_KuipersMunneke"
         M01 = [1.042, 0.0916, 1.734, 0.2039];
 end
+
